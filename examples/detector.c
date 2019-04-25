1e66b90e132c1542afb8ca111889e9cf5ee6162a
tokudaek <tokudaek@gmail.com>
2019-04-25 11:45:39 -0300

    Add function to write detections to file

--- examples/detector.c
+++ examples/detector.c
@@ -1,4 +1,5 @@
 #include "darknet.h"
+#include <libgen.h>
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
 
@@ -343,6 +344,7 @@ void validate_detector_flip(char *datacfg, char *cfgfile, char *weightfile, char
             } else {
                 print_detector_detections(fps, id, dets, num, classes, w, h);
             }
+
             free_detections(dets, num);
             free(id);
             free_image(val[t]);
@@ -602,6 +604,7 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
         //if (nms) do_nms_obj(boxes, probs, l.w*l.h*l.n, l.classes, nms);
         if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
         draw_detections(im, dets, nboxes, thresh, names, alphabet, l.classes);
+
         free_detections(dets, nboxes);
         if(outfile){
             save_image(im, outfile);
@@ -618,6 +621,81 @@ void test_detector(char *datacfg, char *cfgfile, char *weightfile, char *filenam
         free_image(sized);
         if (filename) break;
     }
+}
+
+
+void test_detector_list(char *datacfg, char *cfgfile, char *weightfile,
+		char *listpath, float thresh, float hier_thresh, char *outputdir,
+		int fullscreen)
+{
+	FILE *stream;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+
+	printf("%s\n", listpath);
+	stream = fopen(listpath, "r");
+	if (stream == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
+	}
+
+	list *options = read_data_cfg(datacfg);
+	char *name_list = option_find_str(options, "names", "data/names.list");
+	char **names = get_labels(name_list);
+
+	char outdetections[10000];
+	outdetections[0] = 0;
+
+	image **alphabet = load_alphabet();
+	network *net = load_network(cfgfile, weightfile, 0);
+	set_batch_network(net, 1);
+	srand(2222222);
+
+	while ((nread = getline(&line, &len, stream)) != -1) {
+		if (line[nread - 1] == '\n') line[nread - 1] = 0;
+
+		double time;
+		char buff[256], outcsv[256];
+		char *input = buff;
+		float nms=.45;
+
+		strncpy(input, line, 256);
+
+		image im = load_image_color(input,0,0);
+		if (!im.data) break;
+		image sized = letterbox_image(im, net->w, net->h);
+		layer l = net->layers[net->n-1];
+
+		float *X = sized.data;
+		time=what_time_is_it_now();
+		network_predict(net, X);
+		printf("%s: %f s.\n", input, what_time_is_it_now()-time);
+		int nboxes = 0;
+		detection *dets = get_network_boxes(net, im.w, im.h, thresh, hier_thresh, 0, 1, &nboxes);
+		if (nms) do_nms_sort(dets, nboxes, l.classes, nms);
+
+		get_detections_csv(im, dets, nboxes, thresh, names, alphabet, l.classes,
+				outdetections);
+
+		char *bname = basename(buff);
+		int m = strlen(bname);
+		bname[m-4] = '\0';	// strong assumption: extensions have three letters
+		snprintf(outcsv, 256, "%s/%s.csv", outputdir, bname);
+
+		FILE *outfh = fopen(outcsv, "w");
+		fprintf(outfh, outdetections); //potential segfault
+		fclose(outfh);
+
+		free_detections(dets, nboxes);
+
+		free_image(im);
+		free_image(sized);
+		/*printf("detections:%s\n", outdetections);*/
+	}
+
+	free(line);
+	fclose(stream);
 }
 
 /*
