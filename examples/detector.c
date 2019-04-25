8fb1863b6297e1b3c844dfa06256d74cb4729772
tokudaek <tokudaek@gmail.com>
2019-04-25 12:05:48 -0300

    Skip if csv exists

--- examples/detector.c
+++ examples/detector.c
@@ -1,5 +1,6 @@
 #include "darknet.h"
 #include <libgen.h>
+#include <unistd.h>
 
 static int coco_ids[] = {1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24,25,27,28,31,32,33,34,35,36,37,38,39,40,41,42,43,44,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,67,70,72,73,74,75,76,77,78,79,80,81,82,84,85,86,87,88,89,90};
 
@@ -662,6 +663,15 @@ void test_detector_list(char *datacfg, char *cfgfile, char *weightfile,
 
 		strncpy(input, line, 256);
 
+		char *aux = basename(buff);
+		char bname[128];
+		strncpy(bname, aux, 128);
+		int m = strlen(bname);
+		bname[m-4] = '\0';	// strong assumption: extensions have three letters
+		snprintf(outcsv, 256, "%s/%s.csv", outputdir, bname);
+
+		if(access(outcsv, F_OK ) != -1) continue;
+
 		image im = load_image_color(input,0,0);
 		if (!im.data) break;
 		image sized = letterbox_image(im, net->w, net->h);
@@ -678,10 +688,6 @@ void test_detector_list(char *datacfg, char *cfgfile, char *weightfile,
 		get_detections_csv(im, dets, nboxes, thresh, names, alphabet, l.classes,
 				outdetections);
 
-		char *bname = basename(buff);
-		int m = strlen(bname);
-		bname[m-4] = '\0';	// strong assumption: extensions have three letters
-		snprintf(outcsv, 256, "%s/%s.csv", outputdir, bname);
 
 		FILE *outfh = fopen(outcsv, "w");
 		fprintf(outfh, outdetections); //potential segfault
